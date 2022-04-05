640e5f3c71e12d7ec77fe8168310e8dccd774ea1
Vadim Selivestrau <selivestrau2003@gmail.com>
2022-04-05 16:45:31 +0300

    add a monogame

---
+++ kursovoj/Game1.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MyGame
+{
+    internal class Game1 : Game
+    {
+
+        private GraphicsDeviceManager graphics;
+
+        public Game1()
+            : base()
+        {
+            this.graphics = new GraphicsDeviceManager(this);
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        protected override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            this.GraphicsDevice.Clear(Color.OrangeRed);
+            base.Draw(gameTime);
+        }
+    }
+}
